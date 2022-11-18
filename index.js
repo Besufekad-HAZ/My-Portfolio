d61872d0e441b2a6d2d35ec4247383c2792e22c2
Besufekad Alemu <besurye@gmail.com>
2022-11-18 17:44:52 +0300

    code to debug and review

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const ham_menu = document.querySelector(".ham_menu");
 const links = document.querySelector("#links");
+const sec_nav = document.querySelector("#sec_nav");
 
 ham_menu.addEventListener("click", () => {
   ham_menu.classList.toggle("active");
   links.classList.toggle("active");
+  sec_nav.classList.toggle("active");
 });
 
 document.querySelectorAll(".nv").forEach((n) =>
   n.addEventListener("click", () => {
     ham_menu.classList.remove("active");
     links.classList.toggle("active");
+    sec_nav.classList.toggle("active");
   })
 );
