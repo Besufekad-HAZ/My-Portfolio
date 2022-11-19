420328a00480a7ae6233fddca2bfe480561ae70d
Besufekad Alemu <besurye@gmail.com>
2022-11-20 00:17:46 +0300

    configured eslint

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ const links = document.querySelector("#links");
 const new_bg = document.querySelector("#new_bg");
 
 ham_menu.addEventListener("click", () => {
-    ham_menu.style.display = "none";
+  ham_menu.style.display = "none";
   ham_menu.classList.toggle("active");
   links.classList.toggle("active");
-    mobile_container.style.display = 'block';
+  mobile_container.style.display = "block";
 });
 
 document.querySelectorAll(".nv").forEach((n) =>
   n.addEventListener("click", () => {
     ham_menu.classList.remove("active");
-      ham_menu.style.display = "block";
+    ham_menu.style.display = "block";
     links.classList.toggle("active");
     new_bg.classList.toggle("active");
   })
 );
 
 ham_menu2.addEventListener("click", () => {
-    ham_menu.style.display = "block";
-    // links.style.display = "none";
-    ham_menu.classList.remove("active");
-    links.classList.toggle("active");
+  ham_menu.style.display = "block";
+  // links.style.display = "none";
+  ham_menu.classList.remove("active");
+  links.classList.toggle("active");
 });
