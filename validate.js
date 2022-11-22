e2f0b706e08542081631c5e72cc77713a43e3f3c
Besufekad Alemu <besurye@gmail.com>
2022-11-23 00:32:18 +0300

    first draft on validation for collab

---
+++ validate.js
@@ -0,0 +1,67 @@
+let registrationForm = document.querySelector("#form_all");
+registrationForm.addEventListener("submit", function (event) {
+  if (!validateForm()) event.preventDefault();
+});
+
+let checkUserName = () => {
+  let inputEl = document.querySelector("#name");
+  let inputFeedbackEl = document.querySelector("#reply_name");
+  let regExp = /^[a-zA-Z]{3,20}$/;
+  if (regExp.test(inputEl.value)) {
+    makeValid(inputEl, inputFeedbackEl);
+    return true;
+  } else {
+    makeInValid(inputEl, inputFeedbackEl);
+  }
+  return false;
+};
+
+let checkEmail = () => {
+  let inputEl = document.querySelector("#mail");
+  let inputFeedbackEl = document.querySelector("#reply_mail");
+  let regExp =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (regExp.test(inputEl.value)) {
+    makeValid(inputEl, inputFeedbackEl);
+    return true;
+  } else {
+    makeInValid(inputEl, inputFeedbackEl);
+  }
+  return false;
+};
+
+let checkMessage = () => {
+  let inputEl = document.querySelector("#msg");
+  let inputFeedbackEl = document.querySelector("#reply_message");
+  let regExp = /^[a-zA-Z0-9]{10,500}$/;
+  if (regExp.test(inputEl.value)) {
+    makeValid(inputEl, inputFeedbackEl);
+    return true;
+  } else {
+    makeInValid(inputEl, inputFeedbackEl);
+  }
+  return false;
+};
+
+let makeValid = (inputEl, inputFeedbackEl) => {
+  inputEl.classList.add("valid");
+  inputEl.classList.remove("invalid");
+  inputFeedbackEl.classList.add("correct");
+  inputFeedbackEl.classList.remove("incorrect");
+  inputFeedbackEl.innerText = "Successful";
+};
+
+let makeInValid = (inputEl, inputFeedbackEl) => {
+  inputEl.classList.remove("valid");
+  inputEl.classList.add("invalid");
+  inputFeedbackEl.classList.remove("correct");
+  inputFeedbackEl.classList.add("incorrect");
+  inputFeedbackEl.innerText = `Please Enter ${inputEl.placeholder}`;
+};
+
+let validateForm = () => {
+  var validUsername = checkUserName();
+  var validEmail = checkEmail();
+  var validMessage = checkMessage();
+  return validUsername && validEmail && validMessage;
+};
