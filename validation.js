c006780229242b95d64b9ed458aafb97f363f468
Besufekad Alemu <besurye@gmail.com>
2022-11-23 21:42:14 +0300

    fix eslint error

--- validation.js
+++ validation.js
@@ -1,4 +1,3 @@
-// Validation Form
 document.getElementById("form_all").addEventListener("submit", (ev) => {
   const emailValue = document.getElementById("mail").value;
   const emailError = document.getElementById("error_message");
@@ -6,7 +5,6 @@ document.getElementById("form_all").addEventListener("submit", (ev) => {
   if (emailValue === validate) {
     emailError.textContent = "";
     form.submit();
-    // emptying the local storage
   } else {
     ev.preventDefault();
     emailError.textContent = "Please write your email in lower case!";
