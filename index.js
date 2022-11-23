c9eeda0d8eb3ffea613bd73e656f067d543a56fd
Besufekad Alemu <besurye@gmail.com>
2022-11-23 22:43:19 +0300

    removed unneccesary comments and codes

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const ham_menu = document.querySelector(".ham_menu");
 const mobile_container = document.querySelector(".mobile_menu");
 const ham_menu2 = document.querySelector("#links .ham_menu2");
 const links = document.querySelector("#links");
-// const new_bg = document.querySelector("#link_top");
 
 ham_menu.addEventListener("click", () => {
   ham_menu.style.display = "none";
@@ -25,34 +24,3 @@ ham_menu2.addEventListener("click", () => {
   ham_menu.classList.remove("active");
   links.classList.toggle("active");
 });
-
-// validation
-
-// function validation() {
-//   const form = document.getElementById("form_all");
-//   const name = form.elements.name.value;
-//   const email = form.elements.mail.value;
-//   const message = form.elements.msg.value;
-//   const msg_info = document.getElementById("error_message");
-//   msg_info.innerHTML = "";
-
-//   form.addEventListener("submit", (event) => {
-//      if ((email) !== email.toLowerCase()) {
-//        event.preventDefault();
-//        msg_info.innerHTML = "Please write your email in lower case!";
-//      }
-//     //   else if (message.length < 5) {
-//     //    event.preventDefault();
-//     //    msg_info.innerHTML = "*Please write a message";
-//     //  }
-
-//     //  else if (name.length == 0) {
-//     //    event.preventDefault();
-//     //    msg_info.innerHTML = "Please write your name";
-//     else {
-//        form.submit();
-//     }
-//   });
-// }
-
-// validation();
