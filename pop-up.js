18a617c2f9d84b30fcb8541fdceac1f63ca26b18
Besufekad Alemu <besurye@gmail.com>
2022-11-27 00:17:32 +0300

    modal second sample

--- pop-up.js
+++ pop-up.js
@@ -12,110 +12,118 @@
 
 //second section Declaration Start here
 "use strict";
-const work1 = {
-  name: "Project 1",
-  languages: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
-  description:
-    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus iusto optio non asperiores? Explicabo itaque voluptate, mollitia sequi, delectus nostrum architecto odio exercitationem eius quis quo quam eveniet! Voluptates necessitatibus delectus illo ullam blanditiis.",
-  link: [
-    "https://github.com/Besufekad-HAZ/",
-    "https://besufekad-haz.github.io/My-Portfolio/",
-  ],
-  image: "img/Work 1.png",
-};
+const projects = [
+  {
+    name: "Project 1",
+    languages: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus iusto optio non asperiores? Explicabo itaque voluptate, mollitia sequi, delectus nostrum architecto odio exercitationem eius quis quo quam eveniet! Voluptates necessitatibus delectus illo ullam blanditiis.",
+    link_live: "https://besufekad-haz.github.io/My-Portfolio/",
+    link_source: "https://github.com/Besufekad-HAZ/",
+    image: "Work 1.png",
+  },
 
-const work2 = {
-  name: "Project 2",
-  languages: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
-  description:
-    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus iusto optio non asperiores? Explicabo itaque voluptate, mollitia sequi, delectus nostrum architecto odio exercitationem eius quis quo quam eveniet! Voluptates necessitatibus delectus illo ullam blanditiis.",
-  link: [
-    "https://github.com/Besufekad-HAZ/",
-    "https://besufekad-haz.github.io/My-Portfolio/",
-  ],
-  image: "img/Work 2.png",
-};
-
-const work3 = {
-  name: "Project 3",
-  languages: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
-  description:
-    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus iusto optio non asperiores? Explicabo itaque voluptate, mollitia sequi, delectus nostrum architecto odio exercitationem eius quis quo quam eveniet! Voluptates necessitatibus delectus illo ullam blanditiis.",
-  link: [
-    "https://github.com/Besufekad-HAZ/",
-    "https://besufekad-haz.github.io/My-Portfolio/",
-  ],
-  image: "img/Work 3.png",
-};
-
-const work4 = {
-  name: "Project 4",
-  languages: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
-  description:
-    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus iusto optio non asperiores? Explicabo itaque voluptate, mollitia sequi, delectus nostrum architecto odio exercitationem eius quis quo quam eveniet! Voluptates necessitatibus delectus illo ullam blanditiis.",
-  link: [
-    "https://github.com/Besufekad-HAZ/",
-    "https://besufekad-haz.github.io/My-Portfolio/",
-  ],
-  image: "img/Work 4.png",
-};
-
-const work5 = {
-  name: "Project 5",
-  languages: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
-  description:
-    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus iusto optio non asperiores? Explicabo itaque voluptate, mollitia sequi, delectus nostrum architecto odio exercitationem eius quis quo quam eveniet! Voluptates necessitatibus delectus illo ullam blanditiis.",
-  link: [
-    "https://github.com/Besufekad-HAZ/",
-    "https://besufekad-haz.github.io/My-Portfolio/",
-  ],
-  image: "img/Work 5.png",
-};
-
-const work6 = {
-  name: "Project 6",
-  languages: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
-  description:
-    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus iusto optio non asperiores? Explicabo itaque voluptate, mollitia sequi, delectus nostrum architecto odio exercitationem eius quis quo quam eveniet! Voluptates necessitatibus delectus illo ullam blanditiis.",
-  link: [
-    "https://github.com/Besufekad-HAZ/",
-    "https://besufekad-haz.github.io/My-Portfolio/",
-  ],
-  image: "img/Work 6.png",
-};
+  {
+    name: "Project 2",
+    languages: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus iusto optio non asperiores? Explicabo itaque voluptate, mollitia sequi, delectus nostrum architecto odio exercitationem eius quis quo quam eveniet! Voluptates necessitatibus delectus illo ullam blanditiis.",
+    link_live: "https://besufekad-haz.github.io/My-Portfolio/",
+    link_source: "https://github.com/Besufekad-HAZ/",
+    image: "Work 2.png",
+  },
+  {
+    name: "Project 3",
+    languages: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus iusto optio non asperiores? Explicabo itaque voluptate, mollitia sequi, delectus nostrum architecto odio exercitationem eius quis quo quam eveniet! Voluptates necessitatibus delectus illo ullam blanditiis.",
+    link_live: "https://besufekad-haz.github.io/My-Portfolio/",
+    link_source: "https://github.com/Besufekad-HAZ/",
+    image: "Work 3.png",
+  },
+  {
+    name: "Project 4",
+    languages: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus iusto optio non asperiores? Explicabo itaque voluptate, mollitia sequi, delectus nostrum architecto odio exercitationem eius quis quo quam eveniet! Voluptates necessitatibus delectus illo ullam blanditiis.",
+    link_live: "https://besufekad-haz.github.io/My-Portfolio/",
+    link_source: "https://github.com/Besufekad-HAZ/",
+    image: "Work 4.png",
+  },
+  {
+    name: "Project 5",
+    languages: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus iusto optio non asperiores? Explicabo itaque voluptate, mollitia sequi, delectus nostrum architecto odio exercitationem eius quis quo quam eveniet! Voluptates necessitatibus delectus illo ullam blanditiis.",
+    link_live: "https://besufekad-haz.github.io/My-Portfolio/",
+    link_source: "https://github.com/Besufekad-HAZ/",
+    image: "Work 5.png",
+  },
+  {
+    name: "Project 6",
+    languages: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus iusto optio non asperiores? Explicabo itaque voluptate, mollitia sequi, delectus nostrum architecto odio exercitationem eius quis quo quam eveniet! Voluptates necessitatibus delectus illo ullam blanditiis.",
+    link_live: "https://besufekad-haz.github.io/My-Portfolio/",
+    link_source: "https://github.com/Besufekad-HAZ/",
+    image: "Work 6.png",
+  },
+];
 
 // accessing the projects
-const projects = [work1, work2, work3, work4, work5, work6];
 
-function modal(number) {
+// function modal(number) {
+  const proj_contain = document.querySelectorAll(".project_list");
   const card = document.querySelector("#work");
-  card.classList.toggle("hidden");
-  const name = document.querySelector("#work_name");
-  const list = document.querySelector(".lang-list");
-  list.innerHTML = "";
+//   card.classList.toggle("hidden");
+  const Name = document.querySelector("#work_name");
+  const Languages = document.querySelector(".lang-list");
+  proj_contain.innerHTML = "";
   const img = document.querySelector(".work-img");
   const desc = document.querySelector(".mod-desc");
-  const src_btn = document.querySelector(".source");
   const live_btn = document.querySelector(".live");
+  const src_btn = document.querySelector(".source");
 
-  name.innerHTML = projects[number].name;
+  for (let i = 0; i < projects.length; i++) {
+  var lang = ''
+  projects[i].languages.forEach((tech) => {
+    lang += `
+        <li>${tech}</li>
+      `
+  })
 
-  for (let i = 0; i < projects[number].languages.length; i++) {
-    const li = document.createElement("li");
-    li.textContent = projects[number].languages[i];
-    list.appendChild(li);
-  }
+  
+ proj_contain[i].innerHTML = `
+              <img src='img/${projects[i].image}' alt='Project Image' class='proj_1st_image' id='img${i}' />
+            <div class='proj_list_container'>
+              <h4 class='proj_name'>${projects[i].name}</h4>
+                <ul class='lang_list'>
+                  ${lang}
+                </ul>
+                <button class="proj_view_btn modal-${i}" type="button">See this Project 
+                <i class="fa fa-arrow-right"></i>
+                </button>           
+                 </div> `
+}
 
-  img.setAttribute("src", projects[number].image);
-  src_btn.setAttribute(
-    "onclick",
-    `window.open("${projects[number].link[1]}", "_blank");`
-  );
-  live_btn.setAttribute(
-    "onclick",
-    `window.open("${projects[number].link[0]}", "_blank");`
-  );
-  desc.innerHTML = projects[number].description;
+for (let i = 0; projects.length; i++) {
+   let chkmodal = document.querySelector(`.modal-${i}`);
+   chkmodal.addEventListener("click", () => {
+     let pop_tech = "";
+     projects[i].languages.forEach((tech) => {
+       pop_tech += `
+       <li>${tech}</li>`;
+     });
+     document.querySelector("main").style.display = "none";
+     document.querySelector("header").style.display = "none";
+     Name.innerHTML = projects[i].name;
+     Languages.innerHTML = pop_tech;
+     img.src = "img/" + projects[i].image;
+     desc.innerHTML = projects[i].description;
+     live_btn.setAttribute("href", projects[i].link_live);
+     src_btn.setAttribute("href", projects[i].link_source);
+     card.style.display = "block";
+   });
 }
 
 function toggle() {
@@ -123,5 +131,4 @@ function toggle() {
   card.classList.toggle("hidden");
 }
 
-toggle();
-modal();
+// toggle();
